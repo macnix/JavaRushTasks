145406e2a9bf79e2bc52178a1b5fd3578fdbe038
Oleg Dubetsky <oleg.dubetsky@gmail.com>
2018-08-30 16:25:54 +0300

    +

--- 1.JavaSyntax/src/com/javarush/task/task09/task0922/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0922/Solution.java
@@ -1,10 +1,9 @@
 package com.javarush.task.task09.task0922;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
 
 /* 
 Какое сегодня число?
@@ -13,10 +12,12 @@ import java.util.Locale;
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Date date = new Date(reader.readLine());
+
+        Scanner sc = new Scanner(System.in);
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse(sc.nextLine());
+
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM dd, YYYY", Locale.ENGLISH);
         System.out.println(simpleDateFormat.format(date).toUpperCase());
+
     }
 }
